df69644e7050637c49450ed9e4e4beb7c012fad7
aq <aq@okaq.com>
2020-10-04 19:50:09 -0400

    Add webgl scene

--- nano/zoho.js
+++ nano/zoho.js
@@ -9,6 +9,18 @@ const sce = {
 		sce.a.c = dom.canvas(sce.a.r);
 		dom.add(sce.a.c);
 		console.log(sce.a);
+	},
+	scene() {
+		// webgl
+		sce.b = new THREE.Scene();
+		// cam 
+		sce.c = new THREE.OrthographicCamera(0,1920,0,1080,-1000,1000);
+		// render
+		sce.d = new THREE.WebGLRenderer({canvas:sce.a.c});
+	},
+	debug() {
+		console.log(sce.d.info);
+		console.log(sce.d.getContext());
 	}
 }
 
